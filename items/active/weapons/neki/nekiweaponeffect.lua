9c95deb6101a3dcd1f2bc63e25adc78673bc53da
hyperjuni <93186376+hyperjuni@users.noreply.github.com>
2023-03-26 02:29:59 +0300

    Add files via upload

---
+++ items/active/weapons/neki/nekiweaponeffect.lua
@@ -0,0 +1,20 @@
+require "/scripts/util.lua"
+require "/items/active/weapons/weapon.lua"
+
+local initSB = init
+local updateSB = update
+local uninitSB = uninit
+
+function init()
+	initSB()
+	status.setPersistentEffects("nekiWeaponEffect", {self.primaryAbility.nekiWeaponEffect})
+end
+
+function update(dt, fireMode, shiftHeld)
+	updateSB(dt, fireMode, shiftHeld)
+end
+
+function uninit()
+	uninitSB()
+	status.clearPersistentEffects("nekiWeaponEffect")
+end
