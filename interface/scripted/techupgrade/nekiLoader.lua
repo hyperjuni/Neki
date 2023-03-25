79c628af739d61e3deca7f94f673a524e7850c71
hyperjuni <93186376+hyperjuni@users.noreply.github.com>
2023-03-26 02:11:21 +0300

    Add files via upload

---
+++ interface/scripted/techupgrade/nekiLoader.lua
@@ -0,0 +1,19 @@
+local oldinit = init
+
+function init()
+	if oldinit ~= nil then
+		oldinit()
+	end
+	local customImages = config.getParameter("customBorder")
+	local race = player.species()
+	local gender = player.gender()
+	local path = customImages.path or "/interface/scripted/techupgrade/parts/"
+
+	if customImages and customImages[race] then
+		widget.setImage("imgBackingImage", path .. race .. "Back.png:" .. gender)
+
+		widget.setImage("imgSelectedHead", path .. race .. "Line_" .. gender .. ".png:head")
+		widget.setImage("imgSelectedBody", path .. race .. "Line_" .. gender .. ".png:body")
+		widget.setImage("imgSelectedLegs", path .. race .. "Line_" .. gender .. ".png:legs")
+	end
+end
