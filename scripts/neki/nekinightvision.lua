3f9095a34ffa375c2d51a69504c8b465cdcdcb06
hyperjuni <93186376+hyperjuni@users.noreply.github.com>
2023-12-31 10:42:48 +0200

    0.97.4

--- scripts/neki/nekinightvision.lua
+++ scripts/neki/nekinightvision.lua
@@ -1,45 +1,19 @@
--- Created with the direct assistance of Kherae and TeksFox
+-- Created with the direct assistance of Kherae
 
 local oldUpdate = update
-local oldInit = init
-
-function init(...)
-	if not world.entityExists(entity.id()) then
-		return
-	end
-	if player.species() == "neki" then
-		setParticleConfig(0)
-	else
-		particleConfig = {}
-	end
-	if oldInit then
-		oldInit(...)
-	end
-end
 
 function update(dt,...)
-	if not particleConfig then
-		init()
-	end
-	if player.species() == "neki" then
-		setParticleConfig(dt)
-		localAnimator.spawnParticle(particleConfig)
-	end
-	if oldUpdate then
-		oldUpdate(dt,...)
-	end
-end
-
-function setParticleConfig(dt)
-	if not particleConfig then
-		particleConfig = {
-			light = {125,125,125},
-			type = "textured",
-			image = "/projectiles/invisibleprojectile/invisibleprojectile.png"
-		}
-	end
-	local lightval = math.min(125*status.resourcePercentage("health"),255)
-	particleConfig.light = {lightval,lightval,lightval}
-	particleConfig.position = entity.position()
-	particleConfig.timeToLive = dt
+    if player.species() == "neki" then
+        local lightval = math.min(125*status.resourcePercentage("health"),255)
+        localAnimator.spawnParticle({
+            type = "textured",
+            image = "/projectiles/invisibleprojectile/invisibleprojectile.png",
+            light = {lightval,lightval,lightval},
+            position = entity.position(),
+            timeToLive = dt
+        })
+    end
+    if oldUpdate then
+        oldUpdate(dt,...)
+    end
 end
