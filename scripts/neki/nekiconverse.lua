22a6df5a566b1cdb6dcd57d7461884688681bf68
hyperjuni <93186376+hyperjuni@users.noreply.github.com>
2024-03-24 19:49:42 +0200

    0.98.1

--- scripts/neki/nekiconverse.lua
+++ scripts/neki/nekiconverse.lua
@@ -3,6 +3,9 @@
 neki_speciesDialog = speciesDialog
 
 function speciesDialog(dialog, targetId)
+	if not targetId then
+		return neki_speciesDialog(dialog, targetId)
+	end
 	local otherSpecies = world.entitySpecies(targetId) or "default"
 	if otherSpecies ~= "neki" then
 		return neki_speciesDialog(dialog, targetId)
@@ -11,12 +14,6 @@ function speciesDialog(dialog, targetId)
 	local species = context().species and context().species() or "default"
 	dialog = dialog[species] or dialog.default
 
-	local targetDialog
-	if targetId then
-		targetDialog = dialog[otherSpecies] or dialog.default
-	else
-		targetDialog = dialog.default
-	end
-
+	local targetDialog = dialog[otherSpecies] or dialog.default
 	return targetDialog
 end
