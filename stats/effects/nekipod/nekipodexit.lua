69a2d3ac3745ef9cb218b4131ff28a2cf2621fce
hyperjuni <93186376+hyperjuni@users.noreply.github.com>
2024-05-08 03:05:34 +0300

    0.98.4

--- stats/effects/nekipod/nekipodexit.lua
+++ stats/effects/nekipod/nekipodexit.lua
@@ -1,9 +1,9 @@
 function init()
-  script.setUpdateDelta(5)
+	script.setUpdateDelta(5)
 end
 
 function update(dt)
-  if not status.statusProperty('inPod', false) then
+	if not status.statusProperty('inPod', false) then
 		effect.expire()
 	end
 end
