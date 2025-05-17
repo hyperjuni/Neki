b674da10e9924d2d6b099ad5ec7cb84c61300d1e
hyperjuni <93186376+hyperjuni@users.noreply.github.com>
2025-05-17 22:34:11 +0300

    lightSource instead of particle

--- scripts/neki/nekinightvision.lua
+++ scripts/neki/nekinightvision.lua
@@ -1,19 +1,20 @@
 -- Created by Kherae for Juni <3
+-- Refined by Bottinator22 <3
 
 local oldUpdate = update
 
-function update(dt,...)
+function update(...)
+  localAnimator.clearLightSources()
+  if oldUpdate ~= nil then
+      oldUpdate(...)
+  end
   if player.species() == "neki" then
     local lightval = math.min(125*status.resourcePercentage("health"),255)
-    localAnimator.spawnParticle({
-      type = "textured",
-      image = "/projectiles/invisibleprojectile/invisibleprojectile.png",
-      light = {lightval,lightval,lightval},
-      position = entity.position(),
-      timeToLive = dt
-    })
-  end
-  if oldUpdate then
-    oldUpdate(dt,...)
+    localAnimator.addLightSource(
+      {
+        color = {lightval, lightval, lightval},
+        position = entity.position()
+      }
+    )
   end
 end
