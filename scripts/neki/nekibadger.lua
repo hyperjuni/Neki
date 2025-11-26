d56574337020512c41db923c05503df38d22060a
hyperjuni <93186376+hyperjuni@users.noreply.github.com>
2025-11-26 10:35:48 +0200

    Stripes is not a Neki

--- scripts/neki/nekibadger.lua
+++ scripts/neki/nekibadger.lua
@@ -3,7 +3,7 @@
 neki_hasItemTag = hasItemTag
 
 function hasItemTag(args, board)
-  if npc.species() == "neki" or npc.species() == "nekistripes" and args.tag == "captainschair" then
+  if npc.species() == "neki" and args.tag == "captainschair" then
     return false
   end
   return neki_hasItemTag(args, board)
